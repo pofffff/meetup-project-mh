18712d548f7120b3eb4d68ed8ae1e624bdeac363
Matilda Höglund <48841764+jensengbg-matilda-hoglund@users.noreply.github.com>
2020-09-18 21:39:31 +0200

    navigation guards

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,17 +1,17 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
 module.exports = (req, res, next) => {
-  console.log(" HERE 2")
-  console.log(req.headers)
-  const token = req.headers.authorization
-  if (!token) return res.status(401).json({ message: 'Auth Error' });
+  console.log(" HERE 2");
+  console.log(req.headers);
+  const token = req.headers.authorization;
+  if (!token) return res.status(401).json({ message: "Auth Error" });
 
   try {
-    const decoded = jwt.verify(token, 'secretkey');
+    const decoded = jwt.verify(token, "secretkey");
     req.headers.email = decoded.email;
     next();
   } catch (error) {
     console.error(error);
-    res.status(500).send({ message: 'Invalid Token' });
+    res.status(500).send({ message: "Invalid Token" });
   }
 };
--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,3 +1,32 @@
 <template>
-  <div></div>
+  <div class="nav__wrapper">
+    <ul>
+      <li><h1>JellyfishMeets</h1></li>
+      <li @click="goTo('/user')">Profile</li>
+      <li @click="goTo('/addevent')">NewEvent</li>
+      <li>Events</li>
+      <li @click="logout">Logout</li>
+    </ul>
+  </div>
 </template>
+
+<script>
+export default {
+  data: () => {
+    return {
+
+    }
+  },
+  methods: {
+    goTo(path) {
+      if(this.$route.path !== path) {
+        this.$router.push(path)
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
