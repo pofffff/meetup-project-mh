613e40e0d4e0251d514f689608f42d986799073e
Matilda Höglund <48841764+jensengbg-matilda-hoglund@users.noreply.github.com>
2020-09-18 22:42:48 +0200

    added promise on logout

---
+++ src/views/Profile.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="profile__wrapper">
+        THIS IS USER PAGE
+    </div>
+</template>
+
+<style lang="scss" scoped>
+@import "@/assets/scss/variables";
+
+.profile__wrapper {
+    color: $white;
+font-size: 10em;
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+</style>
